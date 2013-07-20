abc0db34cfce6866afe255fbd8eb3d1568d31436
jjwyse <joshua.wyse@gmail.com>
2013-07-19 21:13:23 -0600

    Changed CSS back

---
+++ src/com/jjw/messagingsystem/controller/InboxController.java
@@ -0,0 +1,11 @@
+package com.jjw.messagingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inbox")
+public class InboxController extends MessagingSystemControllerAbs
+{
+
+}
--- war/resources/views/404.jsp
+++ war/resources/views/404.jsp
@@ -2,6 +2,7 @@
 <head>
 </head>
 <body>
-   <h1>umm...it's gotta be around here somewhere...</h1>
+   <h1>404 Not Found</h1>
+   <h2>umm...it's gotta be around here somewhere...</h2>
 </body>
 </html>
