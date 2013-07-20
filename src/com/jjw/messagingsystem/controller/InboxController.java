76fa752438e023a7c8b545dce85801d452652498
jjwyse <joshua.wyse@gmail.com>
2013-07-19 21:36:25 -0600

    Inbox controller

--- src/com/jjw/messagingsystem/controller/InboxController.java
+++ src/com/jjw/messagingsystem/controller/InboxController.java
@@ -1,11 +1,23 @@
 package com.jjw.messagingsystem.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/inbox")
 public class InboxController extends MessagingSystemControllerAbs
 {
+    private static final Logger myLogger = Logger.getLogger(InboxController.class.getName());
 
+    @RequestMapping(method = RequestMethod.GET)
+    public String getInbox(ModelMap model)
+    {
+        myLogger.info("Handling GET request in inbox controller");
+
+        return VIEW_INBOX;
+    }
 }
