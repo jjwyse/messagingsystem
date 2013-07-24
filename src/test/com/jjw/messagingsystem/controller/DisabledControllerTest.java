d8a297f2bf5015aab4e2987ab8f20cd0eece5b5a
jjwyse <joshua.wyse@gmail.com>
2013-07-23 21:04:39 -0600

    Adding JUnit test cases

---
+++ src/test/com/jjw/messagingsystem/controller/DisabledControllerTest.java
@@ -0,0 +1,16 @@
+package com.jjw.messagingsystem.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DisabledControllerTest
+{
+
+    @Test
+    public void testGetDisabled()
+    {
+        fail("Not yet implemented");
+    }
+
+}
